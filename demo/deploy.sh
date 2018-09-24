c46e6d740e7d66958b54520d231748f772c1a657
Pablo Hirafuji <pablohirafuji@gmail.com>
2018-09-23 22:58:23 -0300

    Fix deploy-key.enc location

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -49,7 +49,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in /deploy-key.enc -out deploy-key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in $DEMO_FOLDER/deploy-key.enc -out deploy-key -d
 
 chmod 600 deploy-key
 eval `ssh-agent -s`
